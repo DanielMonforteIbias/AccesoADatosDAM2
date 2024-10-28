171cd54e00bd7abd298c42e15ab30c669e5d4d8c
Daniel Monforte <daniel.monforteibias@riberadeltajo.es>
2024-10-28 20:34:18 +0100

    28/10/2024 commit
    Additions:
        - Method "VisualizarPedidosCliente" has been added
    TODO:
        - Keep developing methods for EjercicioJardineria

--- Unidad 2/Ejercicios/EjercicioJardineria/src/main/java/ejercicioJardineria/Principal.java
+++ Unidad 2/Ejercicios/EjercicioJardineria/src/main/java/ejercicioJardineria/Principal.java
@@ -1,6 +1,7 @@
 package ejercicioJardineria;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,9 +17,14 @@ public class Principal {
 			int opcion=s.nextInt();
 			switch(opcion) {
 				case 1:
-					insertarEmpleado("Empleado1","Apellido1","Apellido2","1111","empleado1@gmail.com","TAL-ES",1,"Programador");
+					insertarEmpleado("Empleado1","Apellido1","Apellido2","1111","empleado1@gmail.com","TAL-ES",1,"Programador"); //Okay
+					insertarEmpleado("Empleado1","Apellido1","Apellido2","1111","empleado1@gmail.com","TAL-ESSS",1,"Programador"); //Error Oficina
+					insertarEmpleado("Empleado1","Apellido1","Apellido2","1111","empleado1@gmail.com","TAL-ES",100,"Programador"); //Error jefe
 					break;
 				case 2:
+					visualizarPedidosCliente(2); //Cliente no existe
+					visualizarPedidosCliente(6); //Cliente existe sin pedidos
+					visualizarPedidosCliente(4); //Cliente existe con pedidos
 					break;
 				case 3:
 					break;
@@ -59,6 +65,8 @@ public class Principal {
 		System.out.println("7 - Ver los pedidos de todos los clientes");
 		System.out.println("8 - Tratar nuevos empleados");
 		System.out.println("0 - Salir");
+		System.out.println("....................................");
+		System.out.println("------------------------------------");
 	}
 	public static boolean comprobarEmpleado(Connection conexion, int codigoEmpleado) {
 		boolean existe=false;
@@ -98,7 +106,7 @@ public class Principal {
 			insertar=false; //No insertaremos
 			System.out.println("Empleado no insertado. El codigo de jefe no existe"); //Sacamos un mensaje
 		}
-		if(comprobarOficina(conexion,codigoOficina)) { //Si existe la oficina
+		if(!comprobarOficina(conexion,codigoOficina)) { //Si no existe la oficina
 			insertar=false; //No insertaremos
 			System.out.println("Empleado no insertado. El codigo de oficina no existe");
 		}
@@ -132,4 +140,98 @@ public class Principal {
 			}
 		}
 	}
+	public static void visualizarPedidosCliente(int codigoCliente) {
+		try {
+			String sql1="select nombrecliente, lineadireccion1 from clientes where codigocliente=?"; //Obtenemos el cliente
+			PreparedStatement sentencia=conexion.prepareStatement(sql1);
+			sentencia.setInt(1, codigoCliente);
+			ResultSet resul=sentencia.executeQuery();
+			if (resul.next()) { //Si existe el cliente
+				String sql2="select count(*) from pedidos where codigocliente=?";
+				PreparedStatement sentencia2=conexion.prepareStatement(sql2); //Contamos sus pedidos
+				sentencia2.setInt(1, codigoCliente);
+				ResultSet resul2=sentencia2.executeQuery();
+				resul2.next(); //Nos posicionamos, pues siempre devuelve una fila
+				System.out.println("COD-CLIENTE: "+codigoCliente+"           NOMBRE: "+resul.getString(1));
+				System.out.println("DIRECCIÓN1: "+resul.getString(2)+"       Número de pedidos: "+resul2.getInt(1));
+				System.out.println("------------------------------------------------------------------------------------------------------ ");
+				
+				if(resul2.getInt(1)>0) listarPedidosCliente(codigoCliente); //Si tiene pedidos, los listamos
+				
+				resul2.close();
+				sentencia2.close();	
+			}
+			else {
+				System.out.println("------------------------------------------------------------------------------------------------------");
+				System.out.println("Codigo de cliente no existe: "+codigoCliente);
+				System.out.println("------------------------------------------------------------------------------------------------------");
+			}
+			resul.close();
+			sentencia.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void listarPedidosCliente(int codigoCliente) {
+		try {
+			String sql="select codigopedido, fechapedido, estado from pedidos where codigocliente=? order by codigopedido";
+			PreparedStatement sentencia=conexion.prepareStatement(sql);
+			sentencia.setInt(1, codigoCliente);
+			ResultSet resul=sentencia.executeQuery();
+			
+			//Maximos
+			float importeMax=0;
+			int codigoPedidoMax=0, cantidadMaxima=0;
+			Date fechaMax=null;
+			String nombreMaximo="", codigoProductoMax="";
+			
+			while(resul.next()) { //Recorremos los pedidos
+				int codigoPedido=resul.getInt(1);
+				System.out.println("    COD-PEDIDO: "+codigoPedido+"   FECHA PEDIDO: "+resul.getDate(2)+"  ESTADO DEL PEDIDO: "+resul.getString(3));
+				System.out.printf("        %-9s %-9s %-40s %-10s %-10s %-10s %n","NUM-LINEA","COD-PROD","NOMBRE PRODUCTO","CANTIDAD","PREC-UNID","IMPORTE");
+				System.out.printf("        %9s %9s %40s %10s %10s %10s %n","---------","---------","----------------------------------------","----------","----------","----------");
+				int sumaCantidad=0;
+				float sumaPrecio=0, sumaImporte=0;
+				String sql2="select numerolinea, codigoproducto,nombre,cantidad,preciounidad, preciounidad*cantidad from detallepedidos join productos using (codigoproducto) where codigopedido=? order by numerolinea";
+				PreparedStatement sentencia2=conexion.prepareStatement(sql2);
+				sentencia2.setInt(1, codigoPedido);
+				ResultSet resul2=sentencia2.executeQuery();
+				
+				//Recorremos cada detalle del pedido
+				while(resul2.next()) {
+					System.out.printf("        %-9s %-9s %-40s %-10s %-10s %-10s %n",resul2.getInt(1),resul2.getString(2),resul2.getString(3),resul2.getInt(4),resul2.getFloat(5),resul2.getFloat(6));
+					//Sumamos a los totales del pedido
+					sumaCantidad+=resul2.getInt(4);
+					sumaPrecio+=resul2.getFloat(5);
+					sumaImporte+=resul2.getFloat(6);
+					
+					if(resul2.getInt(4)>cantidadMaxima) {
+						cantidadMaxima=resul2.getInt(4);
+						nombreMaximo=resul2.getString(3);
+						codigoProductoMax=resul2.getString(2);
+					}
+				}
+				System.out.printf("        %9s %9s %40s %10s %10s %10s %n","---------","---------","----------------------------------------","----------","----------","----------");
+				System.out.printf("        %-60s %-10s %-10s %-10s %n","TOTALES POR PEDIDO",sumaCantidad,sumaPrecio,sumaImporte);
+				System.out.println();
+				
+				if(sumaImporte>importeMax) {
+					importeMax=sumaImporte;
+					codigoPedidoMax=codigoPedido;
+					fechaMax=resul.getDate(2);
+				}
+				resul2.close();
+				sentencia2.close();
+			}
+			System.out.println("COD de PEDIDO y FECHA PEDIDO CON TOTAL IMPORTE MÁXIMO: "+codigoPedidoMax+",  "+fechaMax);
+			System.out.println("COD PRODUCTO y NOMBRE PRODUCTO, del producto más comprado (producto con CANTIDAD Máxima): "+codigoProductoMax+"   "+nombreMaximo);
+			resul.close();
+			sentencia.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
